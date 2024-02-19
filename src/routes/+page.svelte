834500009ef23a2ae3acb140e8f407e5c71d89da
NoHaxJustCat <78616044+NoHaxJustCat@users.noreply.github.com>
2024-02-19 18:24:16 +0100

    Tailwind setup still no font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-[100%]">
+    <h1 class="">We Build Rockets</h1>
+</div>
