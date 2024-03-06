9230c9ab99e9455afa054413feff3745038c43f0
NoHaxJustCat <78616044+NoHaxJustCat@users.noreply.github.com>
2024-03-06 13:25:56 +0100

    fix for small screens 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -118,7 +118,9 @@
       <div
         class="flex flex-grow h-full text-white justify-center py-12 lg:py-24 px-4 lg:px-20 lg:text-lg sm:text-lg text-sm"
       >
-        <div class="flex flex-col w-full translate-x-1 mb-32 justify-between">
+        <div
+          class="flex flex-col w-full translate-x-1 mb-12 lg:mb-32 justify-between"
+        >
           <div class="flex items-center">
             <div
               class="bg-[#000117] rounded-lg w-full border-2 border-primary-700"
@@ -186,7 +188,7 @@
             </div>
           </div>
         </div>
-        <div class="flex flex-col w-full mt-32 justify-between">
+        <div class="flex flex-col w-full mt-12 lg:mt-32 justify-between">
           <div class="flex items-center">
             <div class="w-6 h-12 lg:w-12 lg:h-24">
               <div
