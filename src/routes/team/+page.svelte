3e0e37b2d17da8d2687d0a41fd562cf6e8fd3746
NoHaxJustCat <78616044+NoHaxJustCat@users.noreply.github.com>
2024-03-06 19:34:08 +0100

    favicon

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,3 +1,3 @@
-<div class="w-full min-h-screen px-64 p-20 bg-[#111827]">
+<div class="w-full min-h-screen px-10 lg:px-64 p-20 bg-[#111827]">
   <h1 class="text-5xl text-white text-center">Work In Progress</h1>
 </div>
