c431052a5eef823bccbe7d60baa3f8a69d4c4720
andreapaoletti03 <andrea0324paoletti@gmail.com>
2024-03-17 12:29:14 +0100

    contact page edit

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,11 +1,73 @@
-<div class="w-full min-h-screen px-20 md:px-64 p-8 lg:p-20 bg-[#02020c]">
-  <h1 class="text-4xl md:text-5xl text-white">Contact Us</h1>
+<div class="w-full min-h-screen px-20 md:px-64 p-8 lg:p-20 bg-[#111827]">
+  <h1 class="text-4xl md:text-5xl text-white">SOCIAL MEDIA AND CONTACTS:</h1>
   <div class="mt-6 lg:mt-12 lg:text-lg font-['Poppins']">
     <span class="text-white">
-      If you need any information or want to offer us a sponsorship you can
-      write us an emai at:
-    </span>
-    <span class="text-primary-700"> info@aurorarocketry.eu </span>
-    <p class="text-white">We usually take from two to three days to answer.</p>
-  </div>
-</div>
+      <b class="text-primary-700" style="font-size: 150%" >Do you want to know more about who we are and what we do?</b> <br>
+      <br>You can follow us on our social media accounts on <span class="text-primary-700"><b>INSTAGRAM</b></span> and <span class="text-primary-700"><b>LINKEDIN</b></span>! Take at look at the posts and stories in our feed and stay tuned 
+      to always be updated with the latest news and activities! <br><br>
+      For information of any kind, regarding the team's origin, its departments or its goals and upcoming projects, you can write us an email at the adress you'll find below... 
+      We usually do not take more than three days to answer! <br>
+      
+      <br>If you're interested in our project, belive in our mission and you want to <span class="text-primary-700"><b>support us and be a part of this journey</b></span>, 
+      please consider the idea of a <span class="text-primary-700"><b>sponsorship</b></span>! 
+      To discuss this possibilty and the mutual benefits it could bring to both of us, don't hesitate
+      to contact our team!
+    </span> 
+    
+    
+
+    
+    <style>
+      
+      #bottom {
+        position: absolute;
+        bottom: 0px;
+        left: 20;
+      }
+    </style>
+ 
+    <div id="bottom">
+        <p class="text-white" style="font-size: 125%"><b><u>Here's how you can reach us:</u></b></p>
+        <span class="text-white"><br>E-mail:
+        <span class="text-primary-700">info@aurorarocketry.eu</span>
+        </span>   
+
+      <style>
+      .container  {
+            display: flex;
+            align-items: center;
+            justify-content: left;
+          }
+          img  {
+            max-width: 100%
+          }
+          .image {
+            flex-basis: 40%
+          }
+          .text {
+            font-size: 18px;
+            padding-right: 1320px;
+          }
+      </style>
+
+      <div class="container">
+        <div class="image">
+          <img src="Instalogo.png" alt="Instagram account" width="60px" height="30px"/>
+        </div>
+        <div class="text">
+          <span class="text-primary-700">@aurorarocketry<span> 
+        </div>
+      </div> 
+
+      <div class="container">
+        <div class="image">
+          <img src="Linkedinlogo.png" alt="Instagram account" width="60px" height="30px"/>
+        </div>
+        <div class="text">
+          <span class="text-primary-700">@aurorarocketry<span> 
+        </div>
+      </div> 
+   </div>
+   
+ </div>
+</div>  
